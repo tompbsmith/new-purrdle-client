802a5f245dfa1b4ebc3e647d9782b176e0dcc54f
lazenbybrowndigital <digital@lazenbybrown.com>
2022-08-09 15:36:49 +0100

    qol

--- pages/index.tsx
+++ pages/index.tsx
@@ -363,6 +363,7 @@ const Index = (props: any) => {
                   return <div key={i}>{i + 1}. {item}</div>
                 })}
               </div>
+              <button onClick={() => { setShowWin(false); }} style={{ marginTop: "20px" }}>Hide</button>
             </div>
           </>
         }
